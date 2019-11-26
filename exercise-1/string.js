62b1d0d86c70f23caef6d3f1967f1e9f90708c6b
Duy-Dung Tran <duy-dung.tran@etu.u-pec.fr>
2019-11-26 13:04:22 +0100

    exercise-snakeCase

---
+++ exercise-1/string.js
@@ -0,0 +1,48 @@
+function ucfirst(str) {
+    if(typeof str !=="string"  || str.length === 0) return "";
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(ucfirst("abc"));
+console.log(ucfirst(null));
+console.log(ucfirst({}));
+
+function capitalize(str) {
+    if(typeof str !== "string"  || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for(let i = 0; i < array.length;i++) {
+        array[i] = ucfirst(array[i]);
+    }
+    return array.join(" ");
+/**
+ * return str 
+ * .toLowerCase()
+ * .split(function(item) {
+ *  return ucfirst(item);
+ * })
+ */
+}
+
+console.log(capitalize("test hello"));
+
+function camelCase(str) {
+    if(typeof str !== "string"  || str.length === 0) return "";
+    var array = capitalize(str);
+    array = array.replace(/[^a-zA-Z0-9-]/g, "");
+    return array;
+}
+
+console.log(camelCase("test      hello       "));
+console.log(camelCase("test    -@  hello !       "));
+
+function snakeCase(str){
+    if(typeof str !== "string"  || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for(let i = 0; i < array.length;i++) {
+        array[i] = array[i].toLowerCase();
+    }
+    return array.join("_"); 
+}
+
+console.log(snakeCase("test Hello@@@"));
+
