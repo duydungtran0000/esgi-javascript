8086adb5ee570432aadf1a8d14d3e25a6299a5f8
Duy-Dung Tran <duy-dung.tran@etu.u-pec.fr>
2019-11-29 16:07:07 +0100

    correction type_check_v1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,7 +1,11 @@
-function type_check_v1(input,type){
-    if ( typeof input ==="function" ) return true;
-    if ( input.constructor === Array && type === "Array" ) return true;
-    return typeof(input) == type;
+function type_check_v1(var1, type) {
+    switch (typeof var1) {
+        case 'object':
+            if (Array.isArray(var1)) return type === "array";
+            if (var1 === null) return type ==="null";
+        default:
+            return typeof var1 === type;
+    }
 }
 
-console.log(type_check_v1(1,"boolean"));
+console.log(type_check_v1([1, 2, 3], "Array"));
