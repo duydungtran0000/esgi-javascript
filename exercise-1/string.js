d9b76339a867c8d863c73d13bbd3fe85197bfa19
duydungtran0000 <duydungtran.0000@gmail.com>
2019-12-18 06:36:55 -0800

    add exercice -3

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -144,3 +144,27 @@ function vig(str, code) {
 }
 
 console.log(vig("wikipedia","crypto"));
+
+const prop_access = (object, propertyPath = '') => {
+    if (propertyPath === '' || propertyPath === null) {
+        return object;
+    }
+    if (object === null) {
+        console.log(object + ' not exist');
+        return;
+    }
+
+    let newObj = object;
+    let path = '';
+    for (let prop of propertyPath.split('.')) {
+        path += '.' + prop;
+        if (newObj.hasOwnProperty(prop)) {
+            newObj = newObj[prop];
+        } else {
+            console.log(path.slice(1) + ' not exist');
+            return;
+        }
+    }
+
+    return newObj
+};
